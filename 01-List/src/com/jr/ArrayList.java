34cd47468252a53e2bba568e7f15837b20e12f5d
wangxiao <wangxiao117210@zongheng.com>
2020-05-12 13:56:09 +0800

    ArrayList 添加方法

---
+++ 01-List/src/com/jr/ArrayList.java
@@ -0,0 +1,121 @@
+package com.jr;
+
+public class ArrayList {
+	/**
+	 * 元素的数量
+	 */
+	private int size;
+	/**
+	 * 存放所有的元素
+	 */
+	private int[] elements;
+	
+	private static final int DEFAULT_CAPATICY = 10;
+	
+	public ArrayList(int capaticy) {
+		elements = new int[capaticy];
+	}
+
+	public ArrayList() {
+		elements = new int[DEFAULT_CAPATICY];
+	}
+	
+	
+	/**
+	 * 清除所有元素
+	 */
+	public void clear() {
+		
+	}
+	
+	/**
+	 * 元素的数量
+	 * @return
+	 */
+	public int size() {
+		return 0;
+	}
+	
+	/**
+	 * 是否为空
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return false;
+	}
+	/**
+	 * 是否包含某元素
+	 * @param element
+	 * @return
+	 */
+	public boolean contains(int element) {
+		return false;
+	}
+	
+	/**
+	 * 添加元素到尾部
+	 * @param element
+	 */
+	public void add(int element) {
+		
+	}
+	
+	/**
+	 * 获取index位置的元素
+	 * @param index
+	 * @return
+	 */
+	public int get(int index) {
+		return 0;
+	}
+	
+	/**
+	 * 设置index位置的元素
+	 * @param index
+	 * @param element
+	 * @return
+	 */
+	public int set(int index, int element) {
+		return 0;
+	}
+	
+	/**
+	 * 在 index 位置插入一个元素
+	 * @param index
+	 * @param element
+	 */
+	public void add(int index, int element) {
+		
+	}
+	
+	/**
+	 * 删除 index 位置的元素
+	 * @param index
+	 * @return
+	 */
+	public int remove(int index) {
+		return 0;
+	}
+	
+	/**
+	 * 查看元素的索引
+	 * @param element
+	 * @return
+	 */
+	public int indexOf(int element) {
+		return -1;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01-List/src/com/jr/Main.java
@@ -0,0 +1,18 @@
+package com.jr;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java!");
+		
+		/// 创建动态数组
+		ArrayList list = new ArrayList();
+		
+		///  动态数组接口
+		
+		
+				
+	}
+
+}
